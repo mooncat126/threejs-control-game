f81c893e518cafefd82e52cc8cb4dfc34885a45b
mooncat126 <xiangyuforwork@gmail.com>
2024-08-18 20:36:39 +0900

    fix: vue config setting

--- office-scene-vue-model/vue.config.js
+++ office-scene-vue-model/vue.config.js
@@ -3,15 +3,26 @@ const path = require("path");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: 'dist/custom-dir', // 更改输出目录
+  publicPath: '/', // 设置 publicPath
   chainWebpack: (config) => {
     const types = ["vue-modules", "vue", "normal-modules", "normal"];
     types.forEach((type) =>
       addStyleResource(config.module.rule("scss").oneOf(type))
     );
     config.plugin('html').tap(args => {
-      args[0].title = 'My Office Scene';
+      args[0].title = 'Your New Title'; // 修改页面标题
       return args;
     });
+
+    config.resolve.alias
+      .set('@components', path.resolve(__dirname, 'src/components'))
+      .set('@assets', path.resolve(__dirname, 'src/assets'));
+
+    config.performance
+      .hints(false)
+      .maxEntrypointSize(512000)
+      .maxAssetSize(512000);
   },
 });
 
@@ -20,6 +31,9 @@ function addStyleResource(rule) {
     .use("style-resource")
     .loader("style-resources-loader")
     .options({
-      patterns: [path.resolve(__dirname, "./src/styles/tools/_sassMagic.scss")],
+      patterns: [
+        path.resolve(__dirname, "./src/styles/tools/_sassMagic.scss"),
+        path.resolve(__dirname, "./src/styles/tools/_variables.scss")
+      ],
     });
 }
