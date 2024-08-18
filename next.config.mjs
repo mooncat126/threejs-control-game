05fe918056467aaa8ad344b3d03788c01717ae20
mooncat126 <xiangyuforwork@gmail.com>
2024-08-18 15:21:14 +0900

    fix: deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/threejs-next-control-game',
-    assetPrefix: '/threejs-next-control-game/',
-    images: {
-      unoptimized: true,
-    },
 };
 
 export default nextConfig;
