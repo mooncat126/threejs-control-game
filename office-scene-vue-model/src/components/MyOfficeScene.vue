e15113778a52d6310d2138a6c06a5fc6576e9313
mooncat126 <xiangyuforwork@gmail.com>
2024-08-18 21:49:09 +0900

    add: cup animation

--- office-scene-vue-model/src/components/MyOfficeScene.vue
+++ office-scene-vue-model/src/components/MyOfficeScene.vue
@@ -3,128 +3,177 @@
 </template>
 
 <script>
-import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { onMounted, getCurrentInstance } from 'vue'
-import { useThemeState } from '@/store'
-import gsap from 'gsap'
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { onMounted, getCurrentInstance } from "vue";
+import { useThemeState } from "@/store";
+import gsap from "gsap";
 
 export default {
-  name: 'MyOfficeScene',
+  name: "MyOfficeScene",
   setup() {
-    const scene = new THREE.Scene()
+    const scene = new THREE.Scene();
     const renderer = new THREE.WebGLRenderer({
       alpha: true,
-    })
-    renderer.useLegacyLights = true
-    renderer.outputEncoding = THREE.sRGBEncoding
-    renderer.shadowMap.enabled = true
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap
+    });
+    renderer.useLegacyLights = true;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    // 添加圆球
+    const sphereGeometry = new THREE.SphereGeometry(0.1, 32, 32);
+    const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xff5a00 });
+
+    const sphere1 = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    const sphere2 = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    const sphere3 = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+    // 调整每个圆球的大小和位置
+    sphere1.scale.set(0.4, 0.4, 0.4);
+    sphere1.position.set(-1, 3.4, -2.9); // 调整到杯子上方
+
+    sphere2.scale.set(0.8, 0.8, 0.8);
+    sphere2.position.set(-0.7, 3.5, -2.9); // 调整到杯子上方
+
+    sphere3.scale.set(0.6, 0.6, 0.6);
+    sphere3.position.set(-0.7, 3.3, -3); // 调整到杯子上方
+
+    // 将圆球添加到场景中
+    scene.add(sphere1);
+    scene.add(sphere2);
+    scene.add(sphere3);
+
+    const cupAnimate = () => {
+      const animateSphere = (sphere, delay) => {
+        gsap.to(sphere.position, {
+          y: `+=0.2`,
+          duration: 0.5,
+          yoyo: true,
+          repeat: -1,
+          ease: "power1.inOut",
+          delay: delay,
+        });
+      };
+
+      // 开始跳动动画
+      animateSphere(sphere1, 0);
+      animateSphere(sphere2, 0.1);
+      animateSphere(sphere3, 0.2);
+    };
+
+    // 添加 XYZ 辅助线
+    // const axesHelper = new THREE.AxesHelper(5); // 5 是辅助线的长度，可以根据需要调整
+    // scene.add(axesHelper);
 
     let chair = null,
       screen = null,
       lampMesh = null,
       macKey = null,
-      originalMaterial = null // 用于存储 mac-keys 的原始材质
+      originalMaterial = null; // 用于存储 mac-keys 的原始材质
 
     const chairAnimate = () => {
       gsap.to(chair.rotation, {
         y: 0.7,
         duration: 10,
-        ease: 'power1.inOut',
+        ease: "power1.inOut",
         repeat: -1,
         yoyo: true,
-      })
-    }
+      });
+    };
 
     const setScreen = () => {
-      const video = document.createElement('video')
-      video.src = '/3D/cat.mp4'
-      video.muted = true
-      video.playsInline = true
-      video.autoplay = true
-      video.loop = true
-      video.play()
-      const videoTexture = new THREE.VideoTexture(video)
-      videoTexture.minFilter = THREE.NearestFilter
-      videoTexture.magFilter = THREE.NearestFilter
-      videoTexture.generateMipmaps = false
-      videoTexture.encoding = THREE.sRGBEncoding
+      const video = document.createElement("video");
+      video.src = "/3D/cat.mp4";
+      video.muted = true;
+      video.playsInline = true;
+      video.autoplay = true;
+      video.loop = true;
+      video.play();
+      const videoTexture = new THREE.VideoTexture(video);
+      videoTexture.minFilter = THREE.NearestFilter;
+      videoTexture.magFilter = THREE.NearestFilter;
+      videoTexture.generateMipmaps = false;
+      videoTexture.encoding = THREE.sRGBEncoding;
       screen.material = new THREE.MeshStandardMaterial({
         map: videoTexture,
-      })
-    }
+      });
+    };
 
-    const gltfLoader = new GLTFLoader()
-    const dracoLoader = new DRACOLoader()
-    dracoLoader.setDecoderPath('/draco/')
-    gltfLoader.setDRACOLoader(dracoLoader)
-    gltfLoader.load('/3D/officeScene.glb', (glb) => {
-      glb.scene.scale.set(1.8, 1.8, 1.8)
-      glb.scene.position.y = -1.5
+    const gltfLoader = new GLTFLoader();
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath("/draco/");
+    gltfLoader.setDRACOLoader(dracoLoader);
+    gltfLoader.load("/3D/officeScene.glb", (glb) => {
+      glb.scene.scale.set(1.8, 1.8, 1.8);
+      glb.scene.position.y = -1.5;
 
       glb.scene.children.forEach((item) => {
-        console.log(item)
-        item.castShadow = true
-        item.receiveShadow = true
+        console.log(item);
+        item.castShadow = true;
+        item.receiveShadow = true;
 
-        if (item.name === 'Chair') {
-          chair = item
-        } else if (item.name === 'mac-screen') {
-          screen = item
-        } else if (item.name === '_台灯') {
-          lampMesh = item
-        } else if (item.name === 'mac-keys') {
-          macKey = item
-          originalMaterial = item.material.clone() // 克隆并保存原始材质
+        if (item.name === "Chair") {
+          chair = item;
+        } else if (item.name === "mac-screen") {
+          screen = item;
+        } else if (item.name === "_台灯") {
+          lampMesh = item;
+        } else if (item.name === "mac-keys") {
+          macKey = item;
+          originalMaterial = item.material.clone(); // 克隆并保存原始材质
         }
-      })
+      });
 
-      chairAnimate()
-      setScreen()
-      scene.add(glb.scene)
-      renderer.render(scene, camera)
-    })
+      chairAnimate();
+      cupAnimate();
 
-    const camera = new THREE.OrthographicCamera()
-    camera.position.set(-9.72, 5.27, -2.25)
-    camera.lookAt(0, 0, 0)
+      setScreen();
+      scene.add(glb.scene);
+      renderer.render(scene, camera);
+    });
 
-    const ambient = new THREE.AmbientLight(0xffffff, 0.5)
-    scene.add(ambient)
+    const camera = new THREE.OrthographicCamera();
+    camera.position.set(-9.72, 5.27, -2.25);
+    camera.lookAt(0, 0, 0);
 
-    const sunLight = new THREE.DirectionalLight(0xffffff)
-    sunLight.castShadow = true
-    sunLight.shadow.camera.far = 20
-    sunLight.shadow.mapSize.set(2048, 2048)
-    sunLight.shadow.normalBias = 0.05
-    sunLight.position.set(-1.5, 7, 3)
-    scene.add(sunLight)
+    const ambient = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambient);
+
+    const sunLight = new THREE.DirectionalLight(0xffffff);
+    sunLight.castShadow = true;
+    sunLight.shadow.camera.far = 20;
+    sunLight.shadow.mapSize.set(2048, 2048);
+    sunLight.shadow.normalBias = 0.05;
+    sunLight.position.set(-1.5, 7, 3);
+    scene.add(sunLight);
 
     // 台灯光源
-    const pointLight = new THREE.PointLight(0xffffff)
-    pointLight.castShadow = true
-    pointLight.position.set(0.6, 4, -2.3)
-    pointLight.shadow.camera.far = 20
-    pointLight.shadow.mapSize.set(2048, 2048)
-    pointLight.shadow.normalBias = 0.05
-    scene.add(pointLight)
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.castShadow = true;
+    pointLight.position.set(0.6, 4, -2.3);
+    pointLight.shadow.camera.far = 20;
+    pointLight.shadow.mapSize.set(2048, 2048);
+    pointLight.shadow.normalBias = 0.05;
+    scene.add(pointLight);
 
     // 添加比桌子稍大的小毯子
     const addCarpet = () => {
-      const carpetGeometry = new THREE.PlaneGeometry(13.5, 12.5) // 调整毯子的大小
-      const carpetMaterial = new THREE.MeshStandardMaterial({ color: 0x8B4513 }) // 设置毯子的颜色（棕色）
-      const carpet = new THREE.Mesh(carpetGeometry, carpetMaterial)
+      const carpetGeometry = new THREE.PlaneGeometry(13.5, 12.5); // 调整毯子的大小
+      const carpetMaterial = new THREE.MeshStandardMaterial({
+        color: 0x8b4513,
+      }); // 设置毯子的颜色（棕色）
+      const carpet = new THREE.Mesh(carpetGeometry, carpetMaterial);
 
-      carpet.rotation.x = -Math.PI / 2 // 使平面水平放置
-      carpet.position.set(0, -1.49, 0) // 使毯子放置在地面上，位置根据场景调整
+      carpet.rotation.x = -Math.PI / 2; // 使平面水平放置
+      carpet.position.set(0, -1.49, 0); // 使毯子放置在地面上，位置根据场景调整
 
-      carpet.receiveShadow = true // 使毯子接收阴影
-      scene.add(carpet)
-    }
-    addCarpet()
+      carpet.receiveShadow = true; // 使毯子接收阴影
+      scene.add(carpet);
+    };
+    addCarpet();
 
     const size = {
       width: 0,
@@ -132,39 +181,39 @@ export default {
       aspect: 1,
       frustrum: 10,
       pixelRatio: Math.min(window.devicePixelRatio, 2),
-    }
+    };
 
-    const pageInstance = getCurrentInstance()
+    const pageInstance = getCurrentInstance();
     const initSize = () => {
-      size.width = pageInstance.refs.divDom.offsetWidth
-      size.height = pageInstance.refs.divDom.offsetHeight
-      size.aspect = size.width / size.height
-      size.pixelRatio = Math.min(window.devicePixelRatio, 3)
-      camera.left = (-size.aspect * size.frustrum) / 2
-      camera.right = (size.aspect * size.frustrum) / 2
-      camera.top = size.frustrum / 2
-      camera.bottom = -size.frustrum / 2
-      camera.updateProjectionMatrix()
-      renderer.setSize(size.width, size.height)
-      renderer.setPixelRatio(size.pixelRatio)
-    }
+      size.width = pageInstance.refs.divDom.offsetWidth;
+      size.height = pageInstance.refs.divDom.offsetHeight;
+      size.aspect = size.width / size.height;
+      size.pixelRatio = Math.min(window.devicePixelRatio, 3);
+      camera.left = (-size.aspect * size.frustrum) / 2;
+      camera.right = (size.aspect * size.frustrum) / 2;
+      camera.top = size.frustrum / 2;
+      camera.bottom = -size.frustrum / 2;
+      camera.updateProjectionMatrix();
+      renderer.setSize(size.width, size.height);
+      renderer.setPixelRatio(size.pixelRatio);
+    };
 
-    const controls = new OrbitControls(camera, renderer.domElement)
-    controls.enableDamping = true // 开启阻尼（惯性）
-    controls.dampingFactor = 0.25 // 阻尼系数
-    controls.screenSpacePanning = false // 禁止摄像机平移
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true; // 开启阻尼（惯性）
+    controls.dampingFactor = 0.25; // 阻尼系数
+    controls.screenSpacePanning = false; // 禁止摄像机平移
 
     // 允许水平和垂直方向的 360 度旋转
-    controls.minPolarAngle = 0 // 垂直旋转的最小角度（0度）
-    controls.maxPolarAngle = Math.PI // 垂直旋转的最大角度（180度）
-    controls.minAzimuthAngle = -Infinity // 水平旋转的最小角度
-    controls.maxAzimuthAngle = Infinity // 水平旋转的最大角度
+    controls.minPolarAngle = 0; // 垂直旋转的最小角度（0度）
+    controls.maxPolarAngle = Math.PI; // 垂直旋转的最大角度（180度）
+    controls.minAzimuthAngle = -Infinity; // 水平旋转的最小角度
+    controls.maxAzimuthAngle = Infinity; // 水平旋转的最大角度
 
     const animate = () => {
-      requestAnimationFrame(animate)
-      controls.update() // 更新控制器状态
-      renderer.render(scene, camera)
-    }
+      requestAnimationFrame(animate);
+      controls.update(); // 更新控制器状态
+      renderer.render(scene, camera);
+    };
 
     // 创建发光材质
     const glowMaterial = new THREE.MeshBasicMaterial({
@@ -172,84 +221,84 @@ export default {
       emissive: 0xadd8e6, // 发光颜色为浅蓝色（接近蓝白光）
       emissiveIntensity: 5, // 发光强度
       transparent: true, // 使材质透明，显示发光部分
-    })
+    });
 
     // 使用useThemeState
-    const themeState = useThemeState()
-    const theme = themeState.theme
+    const themeState = useThemeState();
+    const theme = themeState.theme;
 
     const toggleTheme = () => {
-      themeState.toggleTheme() // 调用store中的切换主题方法
+      themeState.toggleTheme(); // 调用store中的切换主题方法
 
-      if (theme.value === 'light') {
+      if (theme.value === "light") {
         gsap.to(sunLight, {
           intensity: 0.78,
-        })
+        });
         gsap.to(ambient, {
           intensity: 0.78,
-        })
+        });
         gsap.to(pointLight, {
           intensity: 0,
-        })
+        });
         gsap.to(ambient.color, {
           r: 1,
           g: 1,
           b: 1,
-        })
+        });
         if (macKey) {
           macKey.material = originalMaterial; // 开灯时恢复原始材质
         }
       } else {
         gsap.to(sunLight, {
           intensity: 0,
-        })
+        });
         gsap.to(ambient, {
           intensity: 1.03,
-        })
+        });
         gsap.to(pointLight, {
           intensity: 1.68,
-        })
+        });
         gsap.to(ambient.color, {
           b: 0.9568627450980393,
           g: 0.24313725490196078,
           r: 0.3607843137254902,
-        })
+        });
         if (macKey) {
           macKey.material = glowMaterial; // 关灯时切换到发光材质
         }
       }
-    }
+    };
 
-    const raycaster = new THREE.Raycaster()
-    const mouse = new THREE.Vector2()
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
 
     const onClick = (event) => {
-      const rect = renderer.domElement.getBoundingClientRect()
-      mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1
-      mouse.y = -((event.clientY - rect.top) / rect.height) * 2 + 1
+      const rect = renderer.domElement.getBoundingClientRect();
+      mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+      mouse.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
 
-      raycaster.setFromCamera(mouse, camera)
+      raycaster.setFromCamera(mouse, camera);
 
       // 确保 `lampMesh` 已经被正确赋值
       if (lampMesh) {
-        const intersects = raycaster.intersectObject(lampMesh, true)
+        const intersects = raycaster.intersectObject(lampMesh, true);
 
         if (intersects.length > 0) {
-          toggleTheme() // 触发主题切换
+          toggleTheme(); // 触发主题切换
         }
       }
-    }
+    };
 
     onMounted(() => {
-      initSize()
-      animate()
-      pageInstance.refs.divDom.appendChild(renderer.domElement)
-      window.addEventListener('click', onClick)
-    })
+      initSize();
+      animate();
+      pageInstance.refs.divDom.appendChild(renderer.domElement);
+      window.addEventListener("click", onClick);
+    });
 
-    return {}
+    return {};
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
